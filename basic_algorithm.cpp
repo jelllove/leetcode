80fdd6ad375c9e231ce86b62e77d339dcc0df5de
jelllove <jelllove@126.com>
2021-04-01 00:50:24 +0800

    Minor change.

--- basic_algorithm.cpp
+++ basic_algorithm.cpp
@@ -15,13 +15,71 @@ void printArray(T *data, size_t size)
 }
 
 
+int findDuplicate(vector<int>& nums) {
+        //Method 1: using sort
+        /*
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i < nums.size() - 1; ++i)
+        {
+            if (nums[i] == nums[i + 1])
+            {
+                return nums[i];
+            }
+                
+        }
+        */
+        
+        //Method 2: using the hash table
+        /*
+        //pair = value + count
+        unordered_map<int, int> um;
+        
+        for (auto &n : nums)
+        {
+            um[n]++;
+            if (um[n] >= 2)
+                return n;
+        }
+        
+        return 0;
+        */
+        
+        //Method 3: 因为题目限制了这个数组的值只能是从1到N之间
+        //那么可以利用i 从1到n之间进行遍历，然后对相当的index进行一个取负值的操作
+        //如果发现这个值已经被取过一次负值了，那就是重复的
+        //Method 4: 
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        //相当于利用两个快慢指针先找到那个相遇的点，然后，再让另一个回到起点
+        //然后再一起走，这样就可以找到那个相同的点了
+        do
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while (slow != fast);
+        
+        fast = nums[0];
+        
+        while (fast != slow)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return fast;
+        
+        return 0;
+    }
+
+
 int main(int argc, char *argv[])
 {
+    vector<int> vec = {1,3,4,2,2};
 
-    vector<int> vec = {-1,2,1,-4};
+    findDuplicate(vec);
 
-    //printArray(data, sizeof(data) / sizeof(data[0]));
     
     return EXIT_SUCCESS;
-
 }

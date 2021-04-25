a1245c0212cedc5e59c195dcbd4500b156fee4d6
jelllove <jelllove@126.com>
2021-04-26 00:01:44 +0800

    Add new leetcode:leetcode_0122_Best_Time_to_Buy_and_Sell_Stock_II

---
+++ leetcode/leetcode_0122_Best_Time_to_Buy_and_Sell_Stock_II/leetcode_0122_Best_Time_to_Buy_and_Sell_Stock_II.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int start = std::numeric_limits<int>::max();
+        int profit = 0;
+        
+        //其实只要低的时候买入，高的时候卖出，然后如果卖出的时候，也当成是买入的话，就可以了
+        for (auto &p : prices)
+        {
+            if (p > start)
+                profit += (p - start);
+            start = p;
+        }
+        
+        return profit;
+    }
+};

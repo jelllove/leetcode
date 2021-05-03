0b3874b9d35835b5cef3db54a77d913e852905da
jelllove <jelllove@126.com>
2021-05-03 22:09:27 +0800

    Update the 350 subject's solution.

--- leetcode/leetcode_0350_Intersection_of_Two_Arrays_II/leetcode_0350_Intersection_of_Two_Arrays_II.cpp
+++ leetcode/leetcode_0350_Intersection_of_Two_Arrays_II/leetcode_0350_Intersection_of_Two_Arrays_II.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        //Method 1: using the hash table
+        /*
         unordered_map<int, int> um;
         
         vector<int> res;
@@ -18,6 +20,39 @@ public:
             {
                 um[n2]--;
                 res.push_back(n2);
+            }
+        }
+        
+        return res;
+        */
+        
+        
+        //Method 2: using sort + two pointer
+        //先排序
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        
+        //利用两个指针
+        int p1 = 0;
+        int p2 = 0;
+        vector<int> res;
+        
+        while (p1 < nums1.size() && p2 < nums2.size())
+        {
+            //当两个数字一样的时候，就记录一下，否则的话，移动其中那个数字小的指针
+            if (nums1[p1] == nums2[p2])
+            {
+                res.push_back(nums1[p1]);
+                ++p1;
+                ++p2;
+            }
+            else if (nums1[p1] > nums2[p2])
+            {
+                ++p2;
+            }
+            else
+            {
+                ++p1;
             }
         }
         

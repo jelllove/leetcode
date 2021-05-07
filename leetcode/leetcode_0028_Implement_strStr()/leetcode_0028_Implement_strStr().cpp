eca9687689b65707f8a445d1c1062a4efc91fafc
jelllove <jelllove@126.com>
2021-05-08 00:51:03 +0800

    Add the KMP into the subject 0028

--- leetcode/leetcode_0028_Implement_strStr()/leetcode_0028_Implement_strStr().cpp
+++ leetcode/leetcode_0028_Implement_strStr()/leetcode_0028_Implement_strStr().cpp
@@ -1,16 +1,45 @@
 class Solution {
 public:
+    
+    
+    vector<int> getNext(string needle)
+    {
+        if (needle.size() == 0)
+            return vector<int>();
+        
+        vector<int> next(needle.size(), 0);
+        
+        next[0] = 0;
+        
+        for (int q = 1, k = 0; q < needle.size(); ++q)
+        {
+            while (k > 0 && needle[q] != needle[k])
+            {
+                k = next[k - 1];
+            }
+            
+            if (needle[q] == needle[k])
+                ++k;
+            
+            next[q] = k;
+        }
+        
+        return next;
+    }
+    
+    
     int strStr(string haystack, string needle) {
         //Method 1: brust force
-        //如果相同的话，或是查找串为空的话，直接返回0
+        /*
+        //如果主串和模式串相同的话，或是模式串为空的话，直接返回0
         if (haystack == needle || needle == "")
             return 0;
         
         int i = 0;
-        //从0开始一个个遍历被查找串，当剩下的串比查找串还小的话，就不要看了
+        //从0开始一个个遍历主串，当剩下的主串比模式串还小的话，就不要看了
         while (i < haystack.size() && (needle.size() + i) <= haystack.size())
         {
-            //如果被查串的第一个字符与被查找串的第一个字符相符合，就遍历一次查找串
+            //如果主串的第一个字符与模式串的第一个字符相符合，就遍历一次模式串
             if (haystack[i] == needle[0])
             {
                 bool totalMatch = true;
@@ -27,6 +56,38 @@ public:
                     return i;
             }
             ++i;
+        }
+        
+        return -1;
+        */
+        
+        //Method 2: Using KMP algorithm
+        if (haystack == needle || needle == "")
+            return 0;
+        if (needle.size() > haystack.size())
+            return -1;
+        
+        vector<int> next = getNext(needle);
+        
+        int i = 0;
+        int j = 0;
+        
+        while (i < haystack.size() && j < needle.size())
+        {
+            if (haystack[i] == needle[j])
+            {
+                if (j == needle.size() - 1)
+                    return (i - needle.size() + 1);
+                ++j;
+                ++i;
+            }
+            else
+            {
+                if (j > 0)
+                    j = next[j - 1];
+                else
+                    ++i;
+            }
         }
         
         return -1;

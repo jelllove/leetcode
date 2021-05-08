30e5602b4c50a97bf693ac47a061a99ae89325e4
jelllove <jelllove@126.com>
2021-05-08 14:22:50 +0800

    Add new leetcode subject: 234. Palindrome Linked List

---
+++ leetcode/leetcode_0234_Palindrome_Linked_List/leetcode_0234_Palindrome_Linked_List.cpp
@@ -0,0 +1,81 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        //Method 1: record the link into vector
+        /*
+        vector<int> vec;
+        
+        while (head != nullptr)
+        {
+            vec.push_back(head->val);
+            head = head->next;
+        }
+        
+        if (vec.size() == 0)
+            return false;
+        if (vec.size() == 1)
+            return true;
+        
+        int i = 0, j = vec.size() - 1;
+        
+        while (i < j)
+        {
+            if (vec[i] != vec[j])
+                return false;
+            
+            ++i;
+            --j;
+        }
+        
+        return true;
+        */
+        
+        //Method 2: record the link into vector， but optimized
+        /*
+        if (head == nullptr)
+            return false;
+        if (head->next == nullptr)
+            return true;
+            
+        vector<int> vec;
+        
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+        vec.push_back(slow->val);
+        
+        while (fast->next != nullptr && fast->next->next != nullptr)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            vec.push_back(slow->val);
+        }
+        slow = slow->next;
+        
+        if (fast->next != nullptr)
+        {
+            slow = slow->next;
+        }
+        int i = vec.size() - 1;
+        while (slow != nullptr)
+        {
+            if (slow->val != vec[i])
+                return false;
+            slow = slow->next;
+            --i;
+        }
+        
+        return true;
+        */
+        
+    }
+};

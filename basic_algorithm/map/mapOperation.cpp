8bd642a2a72148561765e8af225ebcf4edcb7566
jelllove <jelllove@126.com>
2021-05-18 08:36:38 +0800

    Add the map usage example

---
+++ basic_algorithm/map/mapOperation.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    map<int, string> m;
+
+    m[2] = "jelllove"s;
+    m[3] = "ufo"s;
+    m[4] = "free"s;
+
+
+    
+    //map using the find functionality
+    auto it = m.find(2);
+
+    if (it != m.end())
+    {
+        cout<<it->first<<":"<<it->second<<endl;
+    }
+
+    //using the public find functionality, and using the lamda
+    auto it2 = find_if(m.begin(), m.end(), [](const pair<int, string> &a){return a.second == "ufo";});
+    if (it2 != m.end())
+    {
+        cout<<it2->first<<":"<<it2->second<<endl;
+    }
+
+
+
+    return EXIT_SUCCESS;
+}
+
+

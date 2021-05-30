529c1981d9fe9e04a65fced7ce54d03602b6d19a
jelllove <jelllove@126.com>
2021-05-30 23:49:33 +0800

    Add the new method to resolve the subject 34

--- leetcode/leetcode_0034_Find_First_and_Last_Position_of_Element_in_Sorted_Array/leetcode_0034_Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp
+++ leetcode/leetcode_0034_Find_First_and_Last_Position_of_Element_in_Sorted_Array/leetcode_0034_Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp
@@ -6,6 +6,7 @@ public:
             return vector<int>{-1, -1};
         
         //Method 1: brute force 
+        /*
         int s = -1;
         int e = -1;
         
@@ -26,6 +27,48 @@ public:
                 if (s != -1)
                     break;
             }
+        }
+        
+        return vector<int>{s, e};
+        */
+        
+        //Method 2: binary search
+        int l = 0;
+        int r = nums.size() - 1;
+        
+        int s = -1;
+        int e = -1;
+        
+        while (l < r)
+        {
+            int m = ((r - l)/2) + l;
+            if (nums[m] == target)
+            {
+                s = m;
+                e = m;
+                
+                while ((s - 1) >= 0 && nums[s - 1] == target)
+                    --s;
+                
+                while ((e + 1) < nums.size() && nums[e + 1] == target)
+                    ++e;
+                
+                break;
+            }
+            else if (nums[m] > target)
+            {
+                r = m - 1;
+            }
+            else
+            {
+                l = m + 1;
+            }
+        }
+        
+        if (s == -1 && nums[l] == target)
+        {
+            s = l;
+            e = l;
         }
         
         return vector<int>{s, e};

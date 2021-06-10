2b9994b0a361e2a131e931edb9a8d9a94231d1e9
jelllove <jelllove@126.com>
2021-06-10 17:13:32 +0800

    Modify the 41, using another method, version 2.

--- leetcode/leetcode_0041_First_Missing_Positive/leetcode_0041_First_Missing_Positive.cpp
+++ leetcode/leetcode_0041_First_Missing_Positive/leetcode_0041_First_Missing_Positive.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         //Method 1: sort and find, but low efficiency.
-        
+        /*
         sort(nums.begin(), nums.end());
         
         int minValue = 0;
@@ -23,6 +23,37 @@ public:
         }
         
         return minValue == 0 ? 1 : minValue + 1;
+        */
         
+        //Method 2:  using the min heap
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (auto &n : nums)
+        {
+            if ((pq.empty() || pq.top() != n) && n > 0)
+            {
+                pq.push(n);
+            }
+        }
+        
+        int m = 0;
+        int t = 0;
+        while (!pq.empty())
+        {
+            t = pq.top();
+                        
+            if (t - m > 1)
+            {
+                return m + 1;
+            }
+            else if (t - m == 1)
+            {
+                m = t;
+            }
+            
+            pq.pop();
+        }
+        
+        
+        return m + 1;
     }
 };

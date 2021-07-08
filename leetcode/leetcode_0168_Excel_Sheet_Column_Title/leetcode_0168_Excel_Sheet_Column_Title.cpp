21faa70bf11af567a44c1e77b27cca68f0f72a09
jelllove <jelllove@126.com>
2021-07-09 00:44:22 +0800

    Add the subject 168

---
+++ leetcode/leetcode_0168_Excel_Sheet_Column_Title/leetcode_0168_Excel_Sheet_Column_Title.cpp
@@ -0,0 +1,16 @@
+class Solution {
+
+    
+public:
+    string convertToTitle(int n) {
+        string ans = "";
+        while (n > 0)
+        {
+            char c = 'A' + (n - 1) % 26;
+            ans = c + ans;
+            n = (n - 1) / 26;
+        }
+        
+        return ans;
+    }
+};

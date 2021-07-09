7431137b4c567af17df566975aa9c749aebe0c72
jelllove <jelllove@126.com>
2021-07-10 00:58:03 +0800

    Update the subject 207 METHOD 2

--- leetcode/leetcode_0207_Course_Schedule/leetcode_0207_Course_Schedule.cpp
+++ leetcode/leetcode_0207_Course_Schedule/leetcode_0207_Course_Schedule.cpp
@@ -4,6 +4,7 @@ public:
         if (prerequisites.size() <= 1)
             return true;
         //Method 1: BFS
+        /*
         vector<int> table(numCourses + 1, -1); //index = 某一个课编号， value = {0, 有这个课，但它没有依赖，-1，代表没有这个课，其他的值代表这个课还有依赖}
         for (auto &p : prerequisites)
         {
@@ -49,6 +50,70 @@ public:
         
         
         return (table == vector<int>(numCourses + 1, -1));
+        
+        */
+        
+        //Method 2: BFS optimized
+        //val + rely(vector)
+        unordered_map<int, vector<int> > um;
+        vector<int> table(numCourses, -1);
+        
+        for (auto &p : prerequisites)
+        {
+            if (um.count(p[1]) <= 0)
+            {
+                um[p[1]] = {p[0]};
+            }
+            else
+            {
+                um[p[1]].push_back(p[0]);
+            }
+            
+            
+            if (table[p[0]] == -1)
+                table[p[0]] = 0;
+            table[p[0]]++;
+            
+            if (table[p[1]] == -1)
+                table[p[1]] = 0;
+            
+        }
+        
+        queue<int> q;
+        
+        int total = 0;
+        for (int i = 0; i < table.size(); ++i)
+        {
+            if (table[i] == 0)
+                q.push(i);
+            
+            if (table[i] != -1)
+                ++total;
+        }
+        
+        int done = 0;
+        
+        while (!q.empty())
+        {
+            int val = q.front();
+            q.pop();
+            
+            table[val] = -1;
+            for (auto &r : um[val])
+            {
+                table[r]--;
+                if (table[r] == 0)
+                    q.push(r);
+            }
+            
+            ++done;
+        }
+        
+        return done == total;
+        
+        
+        
+        
         
     }
 };

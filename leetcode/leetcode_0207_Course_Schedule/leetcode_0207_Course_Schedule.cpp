6920a48fa334ecb72945b9c3debd9f3362c8f488
jelllove <jelllove@126.com>
2021-07-10 15:18:57 +0800

    Add the third time optimization for the BFS method for the subject 207

--- leetcode/leetcode_0207_Course_Schedule/leetcode_0207_Course_Schedule.cpp
+++ leetcode/leetcode_0207_Course_Schedule/leetcode_0207_Course_Schedule.cpp
@@ -55,6 +55,7 @@ public:
         
         //Method 2: BFS optimized
         //val + rely(vector)
+        /*
         unordered_map<int, vector<int> > um;
         vector<int> table(numCourses, -1);
         
@@ -110,10 +111,49 @@ public:
         }
         
         return done == total;
+        */
+        
+        //Method 3: BFS + optimized 2
+        vector<vector<int>> data(numCourses, vector<int>{});//代表了一个index是另外的index的输入
+        vector<int> indegree(numCourses, 0);
+        
+        for (auto &p : prerequisites)
+        {
+            data[p[1]].push_back(p[0]);//建立p[0]和p[1]的主从关系
+            indegree[p[0]]++;//入度加1
+        }
+        
+        queue<int> que;
         
         
         
+        for (int i = 0; i < indegree.size(); ++i)
+        {
+            if (indegree[i] == 0)
+            {
+                que.push(i);
+            }
+        }
+        
+        int finishCount = 0;
+        
+        while (!que.empty())
+        {
+            int course = que.front();
+            que.pop();
+            
+            for (int k = 0; k < data[course].size(); ++k)
+            {
+                indegree[data[course][k]]--;
+                if (indegree[data[course][k]] == 0)
+                    que.push(data[course][k]);
+            }
+            ++finishCount;
+        }
         
         
+        return (finishCount == numCourses);
+        
+    
     }
 };

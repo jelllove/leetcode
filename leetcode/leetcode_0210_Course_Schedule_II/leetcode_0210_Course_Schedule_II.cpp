35cfe6c6ec53962f4e3a7bc5b2de74f42eea92d6
jelllove <jelllove@126.com>
2021-07-11 16:11:49 +0800

    Add the leetcode subject 210

---
+++ leetcode/leetcode_0210_Course_Schedule_II/leetcode_0210_Course_Schedule_II.cpp
@@ -0,0 +1,77 @@
+class Solution {
+
+private:
+   
+    
+private:
+    
+    vector<int> generateOrder(stack<int> &st)
+    {
+        vector<int> ret;
+        while (!st.empty())
+        {
+            ret.push_back(st.top());
+            st.pop();
+        }
+        
+        return ret;
+    }
+public:
+    
+    bool helper(int courseID, vector<vector<int>> &adj, vector<bool> &noVisited, vector<bool> &isVisiting, vector<bool> &hasDone, stack<int> &stFinish)
+    {
+        noVisited[courseID] = false;
+        isVisiting[courseID] = true;
+        
+        for (auto &a : adj[courseID])
+        {
+            if (hasDone[a])
+                continue;
+            if (isVisiting[a])
+                return false;
+            
+            if (!helper(a, adj, noVisited, isVisiting, hasDone, stFinish))
+                return false;
+        }
+    
+        isVisiting[courseID] = false;
+        hasDone[courseID] = true;
+        stFinish.push(courseID);
+        
+        return true;
+    }
+    
+    vector<int> findOrder(int n, vector<vector<int>>& p) {
+        
+        stack<int> finishOrder;//使用一个stack来存放一个个课程完成的顺序
+        vector<vector<int>> adj(n, vector<int>{});
+        
+        vector<bool> noVisited(n, true);
+        vector<bool> isVisiting(n, false);
+        vector<bool> hasDone(n, false);
+            
+        
+        //生成邻居关系
+        for (auto &i : p)
+        {
+            adj[i[1]].push_back(i[0]);
+        }
+        
+        //一个个访问
+        for (int k = 0; k < n; k++)
+        {
+            //如果已经被访问过了，就不用了，肯定它肯定已经入栈了
+            if (noVisited[k])
+            {
+                //如果访问时出现了回环之类的，就直接失败
+                if (!helper(k, adj, noVisited, isVisiting, hasDone, finishOrder))
+                    return vector<int>{};
+            }
+            
+        }
+        
+        //最后把stack中的信息反转一下，当然也可以使用vector
+        return generateOrder(finishOrder);
+        
+    }
+};

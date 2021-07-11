d69eff918c2cfc92d5ab473627dc95a7eb0f24ec
jelllove <jelllove@126.com>
2021-07-11 18:30:08 +0800

    Add the subject 148

--- leetcode/leetcode_0148_Sort_List/leetcode_0148_Sort_List.cpp
+++ leetcode/leetcode_0148_Sort_List/leetcode_0148_Sort_List.cpp
@@ -9,6 +9,10 @@
  * };
  */
 
+
+
+class Solution {
+    
 struct Compare
 {
         bool operator () (ListNode* a, ListNode* b)
@@ -17,9 +21,56 @@ struct Compare
         }
 };
 
-class Solution {
+private:
+    ListNode* getMid(ListNode* head)
+    {
+        ListNode *fast = head;
+        ListNode *slow = head;
+
+        ListNode *prev = nullptr;
+        
+        while (fast != nullptr && fast->next != nullptr)
+        {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        prev->next = nullptr;
+        
+        return slow;
+    }
     
     
+    ListNode* merge(ListNode *left, ListNode *right)
+    {
+        ListNode tmp;
+        ListNode *tail = &tmp;
+        
+        while (left != nullptr && right != nullptr)
+        {
+            if (left->val < right->val)
+            {
+                tail->next = left;
+                left = left->next;
+            }
+            else 
+            {
+                tail->next = right;
+                right = right->next;
+            }
+            
+            tail = tail->next;
+            tail->next = nullptr;
+        }
+        
+        if (left != nullptr)
+            tail->next = left;
+        else if (right != nullptr)
+            tail->next = right;
+        
+        return tmp.next;
+    }
     
 public:
     ListNode* sortList(ListNode* head) {
@@ -27,7 +78,8 @@ public:
             return head;
         
         
-        //Method 1: Using the min heap, but it is not efficiency 
+        //Method 1: Using the min heap, but it is not efficiency
+        /*
         priority_queue<ListNode*, vector<ListNode *>, Compare> que;
         
         while (head != nullptr)
@@ -48,6 +100,15 @@ public:
         }
         
         return tmp.next;
+        */
+        
+        //Method 2: merge sort
+        ListNode *mid = getMid(head);
+        ListNode *left = sortList(head);
+        ListNode *right = sortList(mid);
+        
+        return merge(left, right);
+        
         
     }
 };

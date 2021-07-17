eb23f647392063402f3e736b176de63163db31b5
jelllove <jelllove@126.com>
2021-07-18 00:02:58 +0800

    Add the method 2 for the subject 0071

--- leetcode/leetcode_0071_Simplify_Path/leetcode_0071_Simplify_Path.cpp
+++ leetcode/leetcode_0071_Simplify_Path/leetcode_0071_Simplify_Path.cpp
@@ -1,10 +1,10 @@
+
 class Solution {
 public:
     string simplifyPath(string path) {
-        if (path.size() == 0 || path[0] != '/')
-            return "";
         
-        //Method 1: using the stack to store all the 
+        //Method 1: using the stack to store all the path
+        /*
         stack<string> st;
         st.push("/");
         char *p = strtok((char *)path.c_str() + 1, "/");
@@ -38,5 +38,39 @@ public:
         ans = "/"s + ans;
         
         return ans;
+        */
+        
+        //Method 2, using the deque and the istringstream
+        deque<string> deq;
+        stringstream ss(path);
+        
+        string word;
+        while (getline(ss, word, '/'))
+        {
+            if (word.size() != 0)
+            {
+                if (word == "..")
+                {
+                    if (deq.size() > 0)
+                        deq.pop_back();
+                }
+                else if (word != ".")
+                {
+                    deq.push_back(word);
+                }
+            }
+        }
+        
+        
+        string ret = "";
+        while (!deq.empty())
+        {
+            ret += "/"s + deq.front();
+            deq.pop_front();
+        }
+        
+        return (ret.size() == 0) ? "/" : ret;
+        
+        
     }
 };

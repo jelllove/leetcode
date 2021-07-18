246ae738bd6505ce07949f5962b5419e57d7ccf5
jelllove <jelllove@126.com>
2021-07-18 10:37:59 +0800

    Update the subject 64, using the DFS, from logic it is work, but finally time out.

--- leetcode/leetcode_0064_Minimum_Path_Sum/leetcode_0064_Minimum_Path_Sum.cpp
+++ leetcode/leetcode_0064_Minimum_Path_Sum/leetcode_0064_Minimum_Path_Sum.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    
+    void helper(vector<vector<int>> &grid, int i, int j, int rows, int cols, int sum, int &ret)
+    {
+        if (i >= rows || j >= cols)
+            return;
+        if ((i == rows - 1) && (j == cols - 1))
+        {
+            ret = min(ret, sum + grid[i][j]);
+            return;
+        }
+        
+        sum += grid[i][j];
+        helper(grid, i + 1, j, rows, cols, sum, ret);
+        helper(grid, i, j + 1, rows, cols, sum, ret);
+            
+    }
+    
     int minPathSum(vector<vector<int>> grid) {
         if (grid.size() == 0 || grid[0].size() == 0)
             return 0;
@@ -7,7 +24,17 @@ public:
         //get the row and cols amount
         int rows = grid.size();
         int cols = grid[0].size();
-
+        
+        //Method 1: DFS to go through all the possible, but finally it will out of time.
+        /*
+        int ret = std::numeric_limits<int>::max();
+        helper(grid, 0, 0, rows, cols, 0, ret);
+        return ret;
+        */
+        
+        
+        //Method 2: DP, using the m*n matrix as the DP array.
+        
         //generate the dp array
         vector<vector<int>> dp(rows, vector<int>(cols, 0));
         
@@ -32,5 +59,8 @@ public:
         }
         
         return dp[rows - 1][cols - 1];
+        
+        
+        
     }
 };

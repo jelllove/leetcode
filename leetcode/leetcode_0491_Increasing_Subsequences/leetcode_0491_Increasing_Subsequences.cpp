bad85487c703480e422a70baeb46c35f86c904e8
jelllove <jelllove@126.com>
2021-07-20 17:05:20 +0800

    Add the second method for the subject 491

--- leetcode/leetcode_0491_Increasing_Subsequences/leetcode_0491_Increasing_Subsequences.cpp
+++ leetcode/leetcode_0491_Increasing_Subsequences/leetcode_0491_Increasing_Subsequences.cpp
@@ -4,12 +4,36 @@ private:
     vector<vector<int>> ans;
 public:
     
+    void helper(int idx, vector<int>& nums, vector<int> &sub_set)
+    {
+        if (idx >= nums.size())
+            return;
+        
+        unordered_set<int> us;//Record the duplicated num
+        
+        for (int i = idx; i < nums.size(); ++i)
+        {
+            if ((sub_set.size() == 0 || nums[i] >= sub_set.back()) && us.count(nums[i]) == 0)
+            {
+                sub_set.push_back(nums[i]);
+                us.insert(nums[i]);
+                
+                if (sub_set.size() > 1)
+                    ans.push_back(sub_set);
+                
+                helper(i + 1, nums, sub_set);
+                
+                sub_set.pop_back();
+            }
+        }
+    }
     
     vector<vector<int>> findSubsequences(vector<int>& nums) {
         if (nums.size() < 2)
             return {};
         
         //Method 1: using the bit for traverse all possible combination.
+        /*
         vector<int> tmp;
         set< vector<int> > dup;
         
@@ -38,6 +62,12 @@ public:
             }
         }
         
+        return ans;
+        */
+        
+        //Method 2: using the dfs recursion
+        vector<int> sub_set;
+        helper(0, nums, sub_set);
         return ans;
     }
 };

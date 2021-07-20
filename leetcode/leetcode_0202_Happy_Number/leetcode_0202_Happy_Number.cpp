f6757273bf8fc21105415b22d4f04d4e00fd820f
jelllove <jelllove@126.com>
2021-07-20 11:07:12 +0800

    Add the leetcode subject 202

---
+++ leetcode/leetcode_0202_Happy_Number/leetcode_0202_Happy_Number.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        if (n <= 0)
+            return false;
+        
+        //Method 1: using the brute force
+        unordered_set<int> dupNum;//Store the duplicated num, in case the loop
+        
+        while (n != 1)
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                sum += pow((n % 10), 2);
+                n /= 10;
+            }
+            
+            if (dupNum.count(sum) > 0)
+                return false;
+            
+            
+            dupNum.insert(sum);
+            n = sum;
+        }
+        
+        return true;
+        
+    }
+};

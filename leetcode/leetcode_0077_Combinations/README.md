eaa4e534402333c1058798dc0904f10527a20fc3
jelllove <jelllove@126.com>
2021-07-23 16:01:25 +0800

    Add the optimized algorithm for the subject 77

---
+++ leetcode/leetcode_0077_Combinations/README.md
@@ -0,0 +1,4 @@
+这个题目可以使用DFS，也可以使用bit manipulation
+
+![subject](./subject.jpg)
+![0077](./0077手抄.jpg)
--- leetcode/leetcode_0077_Combinations/leetcode_0077_Combinations.cpp
+++ leetcode/leetcode_0077_Combinations/leetcode_0077_Combinations.cpp
@@ -17,10 +17,41 @@ public:
     }
     
     vector<vector<int>> combine(int n, int k) {
+        
+        if (n < 1 || k < 1)
+            return {};
+        
+        
+        
+        //Method 1: using the dfs
+        /*
         vector<vector<int>> ans;
         vector<int> subSet;
-        
         dfs(ans, subSet, 1, n, k);
+        */
+        
+        //Method 2: using the bit manipulation
+        vector<vector<int>> ans;
+        int amount = pow(2, n);
+        vector<int> subSet(k, 0);
+        for (int i = 1; i < amount; ++i)
+        {
+            bitset<20> bt(i);
+            
+            if (bt.count() == k)
+            {
+                int p = 0;
+                for (int b = 0; b < 20; ++b)
+                {
+                    if (bt.test(b))
+                    {
+                        subSet[p++] = b + 1;
+                    }
+                }
+                
+                ans.push_back(subSet);
+            }
+        }
         
         return ans;
     }

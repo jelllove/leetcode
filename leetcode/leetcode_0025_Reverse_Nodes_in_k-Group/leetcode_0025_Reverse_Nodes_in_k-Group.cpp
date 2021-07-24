ba79d71b559e92f2242410881771c7cb7edc031e
jelllove <jelllove@126.com>
2021-07-24 16:24:48 +0800

    Add the subject 25's optimization method 2

--- leetcode/leetcode_0025_Reverse_Nodes_in_k-Group/leetcode_0025_Reverse_Nodes_in_k-Group.cpp
+++ leetcode/leetcode_0025_Reverse_Nodes_in_k-Group/leetcode_0025_Reverse_Nodes_in_k-Group.cpp
@@ -10,11 +10,34 @@
  */
 class Solution {
 public:
+    
+    ListNode *reverseList(ListNode *start, ListNode *end)
+    {
+        ListNode *prev = start;
+        ListNode *tail = start->next;
+        prev->next = nullptr;
+        while (tail != end)
+        {
+            ListNode *tmp = tail->next;
+            tail->next = prev;
+            prev = tail;
+            tail  = tmp;
+        }
+        
+        tail->next = prev;
+        prev = tail;
+        
+        return prev;
+        
+        
+    }
+    
     ListNode* reverseKGroup(ListNode* head, int k) {
         if (head == nullptr || head->next == nullptr || k == 1)
             return head;
         
         //Method 1: go thorugh and store in the vector
+        /*
         vector<ListNode *> vec;
         while (head != nullptr)
         {
@@ -42,5 +65,37 @@ public:
         }
         
         return ans.next;
+        */
+        
+        //Method 2: using the simple pointor
+        if (k <= 1 || head == nullptr || head->next == nullptr)
+            return head;
+        
+        ListNode tmp;
+        ListNode *tail = &tmp;
+        ListNode *start = head;
+        ListNode *end = head;
+        
+        int count = 1;
+        while (end != nullptr)
+        {
+            if (count == k)
+            {
+                ListNode *theNext = end->next;
+                reverseList(start, end);
+                tail->next = end;
+                tail = start;
+                tail->next = theNext;
+                start = end = theNext;
+                count = 1;
+            }
+            else
+            {
+                end = end->next;
+                count++;
+            }
+        }
+        
+        return tmp.next;
     }
 };

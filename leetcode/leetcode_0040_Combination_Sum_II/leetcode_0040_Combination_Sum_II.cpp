2f9eb55a86b92659c4bda817808f5b45db29af6e
jelllove <jelllove@126.com>
2021-07-25 11:21:26 +0800

    Update the subject 40

--- leetcode/leetcode_0040_Combination_Sum_II/leetcode_0040_Combination_Sum_II.cpp
+++ leetcode/leetcode_0040_Combination_Sum_II/leetcode_0040_Combination_Sum_II.cpp
@@ -1,93 +1,29 @@
-/* Method 1: using the recursion
-class Solution {
-
-private:
-    map<vector<int>, bool> ans;
-    
-public:
-    
-    void dfs(vector<int>& candidates, int i, vector<int> &tmp, int target, int n)
-    {
-        if (i >= n && target != 0)
-            return;
-        
-        if (target < 0)
-        {
-            return;
-        }
-        else if (0 == target)
-        {
-            ans[tmp] = true;
-            return;
-        }
-        else
-        {
-            tmp.push_back(candidates[i]);
-            dfs(candidates, i + 1, tmp, target - candidates[i], n);
-            tmp.pop_back();
-            dfs(candidates, i + 1, tmp, target, n);
-        }
-    }
-    
-    
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        ans.clear();
-        
-        if (candidates.size() == 0 || (candidates.size() == 1 && candidates[0] > target))
-        {
-            return vector<vector<int>>{};
-        }
-        
-        
-        sort(candidates.begin(), candidates.end());
-        
-        vector<int> tmp;
-        dfs(candidates, 0, tmp, target, candidates.size());
-        
-        vector<vector<int>> ret;
-        for (auto &a : ans)
-            ret.push_back(a.first);
-        
-        return ret;
-        
-    }
-};
-*/
-
-
+//Method 1: using the recursion
 class Solution {
 
     
 public:
     
-    void helper(vector<int>& candidates, vector<vector<int>> &ans, int index, vector<int> &tmp, int target)
+    void helper(vector<int>& nums, int idx, vector<int> &subSet, int target, vector<vector<int>> &ans)
     {
-        if (index >= candidates.size() && target != 0)
+        if (target == 0)
+        {
+            ans.push_back(subSet);
+            return;
+        }
+        
+        if (idx >= nums.size() || target < 0)
             return;
         
-        if (target < 0)
+        
+        for (int i = idx; i < nums.size(); i++)
         {
-            return;
-        }
-        else if (0 == target)
-        {
-            ans.push_back(tmp);
-            return;
-        }
-        else
-        {
-            for (int i = index; i < candidates.size(); ++i)
-            {
-                //因为前面已经排序过了，所以这里只要比较一下，是不是前一个和当前的是相同的话，那就已经过滤类似{1, 1, 1, 2,5}这种
-                if (i == index || (candidates[i] != candidates[i - 1]))
-                {
-                    tmp.push_back(candidates[i]);
-                    helper(candidates, ans, i + 1, tmp, target - candidates[i]);
-                    tmp.pop_back();
-                }
-            }
+            if (i > idx && nums[i] == nums[i - 1])
+                continue;
             
-            
+            subSet.push_back(nums[i]);
+            helper(nums, i + 1, subSet, target - nums[i], ans);
+            subSet.pop_back();
         }
     }
     
@@ -102,10 +38,9 @@ public:
         sort(candidates.begin(), candidates.end());
         
         vector<vector<int>> ans;
-        vector<int> tmp;
+        vector<int> subSet;
         
-        helper(candidates, ans, 0, tmp, target);
-        
+        helper(candidates, 0, subSet, target, ans);
         
         return ans;
         

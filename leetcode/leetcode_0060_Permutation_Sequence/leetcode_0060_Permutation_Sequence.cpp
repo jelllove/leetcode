8ff9c703a89573a656229b14039b1c963e939c77
jelllove <jelllove@126.com>
2021-07-28 00:06:09 +0800

    Add a new method for the subject 60

--- leetcode/leetcode_0060_Permutation_Sequence/leetcode_0060_Permutation_Sequence.cpp
+++ leetcode/leetcode_0060_Permutation_Sequence/leetcode_0060_Permutation_Sequence.cpp
@@ -29,6 +29,8 @@ public:
     
     string getPermutation(int n, int k) {
         
+        //Method 1: using the buste force
+        /*
         if (n == 1)
             return "1"s;
         
@@ -38,7 +40,15 @@ public:
             amount *= i;
         }
         
-        int reset = k;
+        int reset = 0;
+        
+        if (k <= amount)
+            reset = k - 1;
+        else
+            reset = (k - 1) % amount;
+            
+        
+        
         
         //if ((amount - 1) != 1 && (k != (amount - 1)))
         //    reset = k % (amount - 1);
@@ -63,6 +73,33 @@ public:
         }
         
         return ans;
+        */
+        
+        //Method 2: using the math algorithm
+        string nums = "123456789";
+        vector<int> f(n, 1);
+        
+        //把每一个n对应的阶乘都先算出来
+        for (int i = 1; i < n; ++i)
+            f[i] = f[i - 1] * i;
+        
+        string res = "";
+        --k;
+        while (n >= 1)
+        {
+            //先算取要从nums中取第几个值
+            int a = k / f[n - 1];
+            //把k改成下一次的，因为取了一次之后，下次就要从小的里去取了
+            //比如n = 7, k = 17，那取了第一个数之后，因为要从剩下的N=3中去取了，所以要取模
+            k %= f[n - 1];
+            //把对应的nums的值放到数组中
+            res.push_back(nums[a]);
+            //同时从nums中删除它
+            nums.erase(a, 1);
+            --n;
+        }
+        
+        return res;
         
     }
 };

d483bfc8a3d011dce09f8c1b1c3910ed919189c2
jelllove <jelllove@126.com>
2021-07-31 12:40:00 +0800

    Add the picture for subject 43, and add the optimized method for it.

--- leetcode/leetcode_0043_Multiply_Strings/leetcode_0043_Multiply_Strings.cpp
+++ leetcode/leetcode_0043_Multiply_Strings/leetcode_0043_Multiply_Strings.cpp
@@ -66,6 +66,9 @@ public:
     }
     
     string multiply(string num1, string num2) {
+        
+        //Method 1: using the brute force, low efficiency.
+        /*
         string strSmall = num1.size() > num2.size() ? num2 : num1;
         
         string strBig = num1.size() <= num2.size() ? num2 : num1;
@@ -88,5 +91,45 @@ public:
         }
         
         return "0";
+        */
+        
+        //Method 2: using the math pattern
+        if (num1.size() == 0 || num2.size() == 0)
+            return "0";
+        
+        int m = num1.size();
+        int n = num2.size();
+        vector<int> res(m + n, 0);
+        
+        for (int i = m - 1; i >= 0; --i)
+        {
+            for (int j = n - 1; j >= 0; --j)
+            {
+                res[i + j + 1] += (num1[i] - '0') * (num2[j] - '0');
+            }
+        }
+        
+        int carry = 0;
+        int k = m + n - 1;
+        while (k >= 0)
+        {
+            res[k] += carry;
+            carry = res[k] / 10;
+            res[k] %= 10;
+            --k;
+        }
+        
+        k = 0;
+        while (k < (m + n) && res[k] == 0)
+            ++k;
+        
+        string ans = "";
+        while (k < (m + n))
+        {
+            ans += (res[k] + '0');
+            ++k;
+        }
+        
+        return (ans == "") ? "0" : ans;
     }
 };

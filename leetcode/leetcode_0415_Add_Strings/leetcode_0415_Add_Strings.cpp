c9238348ed1ac9733205568ae8b22b9b3e78f881
jelllove <jelllove@126.com>
2021-08-06 20:16:16 +0800

    Update the old subject

--- leetcode/leetcode_0415_Add_Strings/leetcode_0415_Add_Strings.cpp
+++ leetcode/leetcode_0415_Add_Strings/leetcode_0415_Add_Strings.cpp
@@ -1,35 +1,41 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        int pro = 0;
-        int i = num1.size() - 1;
-        int j = num2.size() - 1;
         
-        string ans = "";
+        if (num1.size() == 0)
+            return num2;
         
-        while (i >= 0 || j >= 0 || pro > 0)
+        if (num2.size() == 0)
+            return num1;
+        
+        string res(max(num1.size(), num2.size()) + 1, '0');
+        
+        int carry = 0;
+        int n1 = num1.size() - 1;
+        int n2 = num2.size() - 1;
+        int idx = res.size() - 1;
+        while (n1 >= 0 || n2 >= 0 || carry != 0)
         {
-            int tmp = pro;
-            pro = 0;
-            
-            if (i >= 0)
+            if (n1 >= 0)
             {
-                tmp += (num1[i] - '0');
-                --i;
+                carry += (num1[n1] - '0');
+                --n1;
             }
             
-            if (j >= 0)
+            if (n2 >= 0)
             {
-                tmp += (num2[j] - '0');
-                --j;
+                carry += (num2[n2] - '0');
+                --n2;
             }
             
-            pro = tmp / 10;
-            tmp %= 10;
-            
-            ans = to_string(tmp) + ans;
+            res[idx] = carry % 10 + '0';
+            carry /= 10;
+            --idx;
         }
         
-        return ans;
+        return res.substr(idx + 1, res.size() - idx + 1);
+        
+        
+        
     }
-};
+}; 

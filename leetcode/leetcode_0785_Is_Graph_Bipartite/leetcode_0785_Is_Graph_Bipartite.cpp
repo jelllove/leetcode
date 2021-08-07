bbc93081bfc5de9200cb1c5139dfa162dcfc9738
jelllove <jelllove@126.com>
2021-08-08 01:01:08 +0800

    Add the subject 785

---
+++ leetcode/leetcode_0785_Is_Graph_Bipartite/leetcode_0785_Is_Graph_Bipartite.cpp
@@ -0,0 +1,38 @@
+class Solution 
+{
+public:
+    bool helper(vector<vector<int>>&graph,vector<int>&colour,int curr)
+    {
+        for(auto it:graph[curr])
+        {
+            if(colour[it]==colour[curr])
+            {
+                return false;
+            }
+            else if(colour[it]==0)
+            {
+                colour[it]=-colour[curr];
+                if(!helper(graph,colour,it))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) 
+    {
+        int n=graph.size();
+        vector<int>colour(n,0);
+        for(int i=0;i<n;i++)
+        {
+            if(colour[i]==0) 
+            {
+                colour[i]=-1;
+                if(!helper(graph,colour,i))
+                    return false;
+            }
+        }
+        return true;
+    }
+};

ef73369fb37d4f69ecf6348737ac09101b65bfcb
jelllove <jelllove@126.com>
2021-09-29 09:10:21 +0800

    Update the 268 subject

--- leetcode/leetcode_0268_Missing_Number/leetcode_0268_Missing_Number.cpp
+++ leetcode/leetcode_0268_Missing_Number/leetcode_0268_Missing_Number.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        //Method 1: using the total amount to find out the missed number
+        /*
         int total = int(double(nums.size() + 1) * (double(nums.size()) / 2.0));
         
         int sum = 0;
@@ -10,5 +12,20 @@ public:
         }
         
         return total - sum;
+        */
+
+       //Method 2: Using the xor method to find out the missed number
+       int ans = 0;
+        
+        int idx = 0;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            ans ^= nums[i];
+            ans ^= idx++;
+        }
+        
+        ans ^= idx;
+        
+        return ans;
     }
 };

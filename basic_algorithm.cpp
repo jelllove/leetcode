f308be6cc975c83775119e3618b9f308bac581bb
jelllove <jelllove@126.com>
2021-10-07 10:09:54 +0800

    Update the basic algorithm.

--- basic_algorithm.cpp
+++ basic_algorithm.cpp
@@ -1,14 +1,53 @@
 #include "include/public.h"
-#include <stdlib.h>
-#include <string.h>
-#include <memory>
+
+class Solution {
+public:
+    
+    bool splitString(string &str, string declime, vector<string> &res)
+    {
+        char *p = strtok((char *)str.c_str(), declime.c_str());
+        
+        while (p != nullptr)
+        {
+            res.push_back(p);
+            p = strtok(nullptr, declime.c_str());
+        }
+        
+        return res.size() != 0;
+    }
+    
+    void dfs(vector<string> &res, int &idx)
+    {
+        if (idx >= res.size())
+            return;
+        
+        if (res[idx] == "#")
+            return;
+        
+        dfs(res, ++idx);
+        dfs(res, ++idx);
+    }
+    
+    
+    bool isValidSerialization(string preorder) {
+        
+        vector<string> res;
+        if (!splitString(preorder, ",", res) )
+            return false;
+        
+        int idx = 0;
+        dfs(res, idx);
+        if (idx >= res.size())
+            return true;
+        else
+            return false;
+    }
+};
 
 int main(int argc, char *argv[])
 {
-    
-    vector<int> vec = {3,4,2,2,2};
 
-    
-    
+    Solution().isValidSerialization("9,3,4,#,#,1,#,#,2,#,6,#,#");
+
     return EXIT_SUCCESS;
 }
